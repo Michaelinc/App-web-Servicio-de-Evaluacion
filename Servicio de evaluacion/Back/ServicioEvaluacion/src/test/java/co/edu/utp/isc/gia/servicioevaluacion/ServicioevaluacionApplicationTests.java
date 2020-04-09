c2a49a313341b67cafcb106468f34979cfdbd6a4
Michael Rendon <michael.rendon@utp.edu.co>
2020-04-09 18:20:12 -0500

    Inicializando el repositorio y creando proyecto de back con todas las dependencias necesarias

---
+++ Servicio de evaluacion/Back/ServicioEvaluacion/src/test/java/co/edu/utp/isc/gia/servicioevaluacion/ServicioevaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.servicioevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioevaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
